92cff3ed1edb840a1a80b66dccd3e8cffbde1492
Tim van de Wal <timvandewal@gmail.com>
2017-07-04 19:27:39 +0200

    Made first changes to implement REST

---
+++ app/src/main/java/com/example/tim/androidshowcase/RestClientActivity.java
@@ -0,0 +1,13 @@
+package com.example.tim.androidshowcase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_client);
+    }
+}
