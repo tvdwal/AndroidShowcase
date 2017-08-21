e91567dd5dae2a7b4b939df49b715fd0cc7e6ddd
VAN DE WAL Tim <timvandewal@gmail.com>
2017-08-21 16:45:52 +0200

    Extra files for fragment activity & initial activity setup

---
+++ app/src/main/java/com/example/tim/androidshowcase/Fragments/PlanetImageFragment.java
@@ -0,0 +1,23 @@
+package com.example.tim.androidshowcase.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tim.androidshowcase.R;
+
+/**
+ * Created by tvandewal on 21-8-2017.
+ */
+
+public class PlanetImageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+        return view;
+    }
+}
