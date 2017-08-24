826bdf718838cdbffd773102c9d93ec709482857
VAN DE WAL Tim <timvandewal@gmail.com>
2017-08-24 15:28:55 +0200

    Added planet images to resources folder & Created fragment coordination between planet selection & planet description

---
+++ app/src/main/java/com/example/tim/androidshowcase/Fragments/PlanetFragmentCoordinator.java
@@ -0,0 +1,9 @@
+package com.example.tim.androidshowcase.Fragments;
+
+/**
+ * Created by tvandewal on 24-8-2017.
+ */
+
+public interface PlanetFragmentCoordinator {
+    void onPlanetSelectionChanged(int index);
+}
