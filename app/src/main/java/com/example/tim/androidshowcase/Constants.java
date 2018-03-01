60efc0dba809874ee7b926e3f01d2ee723b69f19
VAN DE WAL Tim <tvandewal@europe.corp.altran.com>
2018-03-01 13:50:52 +0100

    Added  SharedPreference Activity including functionality

---
+++ app/src/main/java/com/example/tim/androidshowcase/Constants.java
@@ -0,0 +1,11 @@
+package com.example.tim.androidshowcase;
+
+/**
+ * Created by tvandewal on 1-3-2018.
+ */
+
+public class Constants {
+
+    public static final String PREF_FILE_NAME = ".my_pref_file";
+    public static final String LATEST_ACTIVITY = "LATEST_ACTIVITY";
+}
