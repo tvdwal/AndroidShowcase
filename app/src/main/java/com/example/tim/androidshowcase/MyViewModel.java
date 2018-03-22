126614849e33f8b1872ef83efe71d042405f4b14
Tim van de Wal <timvandewal@gmail.com>
2018-03-22 21:45:48 +0100

    Added ViewModel Dependency

---
+++ app/src/main/java/com/example/tim/androidshowcase/MyViewModel.java
@@ -0,0 +1,11 @@
+package com.example.tim.androidshowcase;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Tim on 22-3-2018.
+ */
+
+public class MyViewModel extends ViewModel {
+
+}
