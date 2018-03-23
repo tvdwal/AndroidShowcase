584b2dc090fec647f6b544b708cafde64408b041
Tim van de Wal <timvandewal@gmail.com>
2018-03-23 18:57:55 +0100

    WIP: Added fragment transaction for floating action button in RealmActivity

--- app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
+++ app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
@@ -1,11 +1,15 @@
 package com.example.tim.androidshowcase.Activities
 
+import android.app.FragmentManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.example.tim.androidshowcase.R
+import com.example.tim.androidshowcase.Realm.RealmNewItemFragment
 
 import kotlinx.android.synthetic.main.activity_realm.*
+import kotlinx.android.synthetic.main.content_realm.*
 
 class RealmActivity : AppCompatActivity() {
 
@@ -14,10 +18,17 @@ class RealmActivity : AppCompatActivity() {
         setContentView(R.layout.activity_realm)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            showFragment(RealmNewItemFragment())
         }
+    }
+
+    fun showFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack("NewItem")
+                .commit()
     }
 
 }
