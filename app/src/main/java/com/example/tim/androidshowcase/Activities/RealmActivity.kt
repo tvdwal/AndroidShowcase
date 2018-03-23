030b0d5a4e9cbff1c4e6f451bb07d52c9c8ab747
Tim van de Wal <timvandewal@gmail.com>
2018-03-23 19:11:21 +0100

    Fixed fab & fragment display

--- app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
+++ app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
@@ -1,15 +1,13 @@
 package com.example.tim.androidshowcase.Activities
 
-import android.app.FragmentManager
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.example.tim.androidshowcase.R
 import com.example.tim.androidshowcase.Realm.RealmNewItemFragment
+import com.example.tim.androidshowcase.Realm.RealmOverviewFragment
 
 import kotlinx.android.synthetic.main.activity_realm.*
-import kotlinx.android.synthetic.main.content_realm.*
 
 class RealmActivity : AppCompatActivity() {
 
@@ -18,15 +16,19 @@ class RealmActivity : AppCompatActivity() {
         setContentView(R.layout.activity_realm)
         setSupportActionBar(toolbar)
 
+        showFragment(RealmOverviewFragment())
+
         fab.setOnClickListener {
-            showFragment(RealmNewItemFragment())
+            val fragment = RealmNewItemFragment()
+            showFragment(fragment)
+            fab.setImageResource(fragment.getFabIcon())
         }
     }
 
     fun showFragment(fragment: Fragment) {
         supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.container, fragment)
+                .replace(R.id.layoutRealmFragmentContainer, fragment)
                 .addToBackStack("NewItem")
                 .commit()
     }
--- app/src/main/java/com/example/tim/androidshowcase/Realm/RealmOverviewFragment.kt
+++ app/src/main/java/com/example/tim/androidshowcase/Realm/RealmOverviewFragment.kt
@@ -1,7 +1,7 @@
 package com.example.tim.androidshowcase.Realm
 
-import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,8 +13,17 @@ import com.example.tim.androidshowcase.R
 
 class RealmOverviewFragment : Fragment() {
 
+    companion object {
+        val fab_icon = R.drawable.ic_home_white_48dp
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view: View = inflater!!.inflate(R.layout.fragment_realm_overview, container, false)
         return view
     }
+
+    fun getFabIcon(): Int {
+        return fab_icon
+    }
+
 }
