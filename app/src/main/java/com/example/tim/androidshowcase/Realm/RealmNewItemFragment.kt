2949e2e226932a8433655eaa4044be092536a275
Tim van de Wal <timvandewal@gmail.com>
2018-03-23 18:32:26 +0100

    Added 2 fragment kotlin classes

---
+++ app/src/main/java/com/example/tim/androidshowcase/Realm/RealmNewItemFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tim.androidshowcase.Realm
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tim.androidshowcase.R
+
+/**
+ * Created by Tim on 23-3-2018.
+ */
+
+class RealmNewItemFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view: View = inflater!!.inflate(R.layout.fragment_realm_new_item, container, false)
+        return view
+    }
+}
