1c85edf979c490077bb617ae7fe93971e0bd7568
Tim van de Wal <timvandewal@gmail.com>
2018-03-23 17:42:14 +0100

    Added RealmActivity & layouts

--- app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
+++ app/src/main/java/com/example/tim/androidshowcase/Activities/RealmActivity.kt
@@ -1,5 +1,23 @@
 package com.example.tim.androidshowcase.Activities
 
-/**
- * Created by Tim on 23-3-2018.
- */
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.example.tim.androidshowcase.R
+
+import kotlinx.android.synthetic.main.activity_realm.*
+
+class RealmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realm)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
