e1f854238752d351c3bef44bdd2e3fdad1357ced
Tim van de Wal <timvandewal@gmail.com>
2018-03-23 18:21:35 +0100

    Rewrote MainActivity to Kotlin & Added 2 new fragments for RealmActivity

---
+++ app/src/main/java/com/example/tim/androidshowcase/Activities/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.tim.androidshowcase.Activities
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import com.example.tim.androidshowcase.Constants
+import com.example.tim.androidshowcase.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.default_toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setToolbar()
+        setButtons()
+        setLatestActivity()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.default_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var msg = ""
+
+        when (item.itemId) {
+            R.id.id_menu_item_account -> msg = item.toString()
+            R.id.id_menu_item_android -> msg = item.toString()
+            R.id.id_menu_item_delete -> msg = item.toString()
+            R.id.id_menu_item_help -> msg = item.toString()
+            R.id.id_menu_item_home -> msg = item.toString()
+            R.id.id_menu_item_settings -> msg = item.toString()
+        }
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(default_toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
+        default_toolbar.title = "Welcome"
+        default_toolbar.subtitle = "Tim"
+    }
+
+    private fun setButtons() {
+        btnRecyclerViewSharedElement.setOnClickListener { goToActivity(RecycleViewDetailedActivity::class.java) }
+        btnRestClient.setOnClickListener { goToActivity(RestClientActivity::class.java) }
+        btnFragments.setOnClickListener { goToActivity(FragmentsActivity::class.java) }
+        btnViewModel.setOnClickListener { goToActivity(ViewModelActivity::class.java) }
+        btnSharedPreferences.setOnClickListener { goToActivity(SharedPreferenceActivity::class.java) }
+        btnRealm.setOnClickListener{ goToActivity(RealmActivity::class.java) }
+    }
+
+    private fun goToActivity(destinationActivity: Class<*>) {
+        val i = Intent(this@MainActivity, destinationActivity)
+        startActivity(i)
+    }
+
+    private fun setLatestActivity() {
+        val sharedPreferences = getSharedPreferences(packageName + Constants.PREF_FILE_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(Constants.LATEST_ACTIVITY, "MAIN_ACTIVITY")
+        editor.apply()
+    }
+
+}
