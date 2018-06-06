18226e4f985b09c7e18420134baceb46eaf17229
Tim van de Wal <tim@appautomatic.nl>
2018-06-06 19:09:05 +0200

    Refactored loginActivity to loginFragment

--- app/src/main/java/com/example/tim/androidshowcase/Login/LoginActivity.kt
+++ app/src/main/java/com/example/tim/androidshowcase/Login/LoginActivity.kt
@@ -1,102 +1,23 @@
 package com.example.tim.androidshowcase.Login
 
-import android.arch.lifecycle.ViewModelProviders
-import android.content.Context
-import android.content.Intent
-import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.android.databinding.library.baseAdapters.BR
-import com.example.tim.androidshowcase.MainActivity
 import com.example.tim.androidshowcase.R
-import com.example.tim.androidshowcase.databinding.ActivityLoginBinding
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_login.*
-
 
 class LoginActivity : AppCompatActivity() {
 
     companion object {
-        private const val prefsFile = ".login_prefs"
-        private const val pref_tag_latest_name = "LOGIN_NAME"
-        private const val pref_tag_latest_pass = "LOGIN_PASS"
-        private const val pref_tag_remember_me = "LOGIN_REMEMBER"
         const val TAG = "LoginActivity"
     }
 
     lateinit var loginViewModel: LoginViewModel
-    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        setBindings()
-        loadLoginDetails()
-
-        buttonLogin.setOnClickListener {
-            loginViewModel.loginRemember.value = checkBoxLogin.isChecked
-            loginViewModel.loginName.value = editTextLoginName.text.toString()
-            loginViewModel.loginPass.value = editTextLoginPass.text.toString()
-            loginAs(loginViewModel.loginName.value!!, loginViewModel.loginPass.value!!)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        saveLoginDetails()
-    }
-
-    private fun loginAs(name: String, pass: String) {
-        firebaseAuth.signInWithEmailAndPassword(name, pass).addOnCompleteListener(this@LoginActivity, {
-            task -> run {
-                if (task.isSuccessful) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                } else {
-                    Toast.makeText(this, "Could not login as " + loginViewModel.loginName.value, Toast.LENGTH_SHORT).show()
-                }
-            }
-
-        })
-    }
-
-    private fun setBindings() {
-        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        binding.setVariable(BR.loginViewModel, loginViewModel)
-        binding.executePendingBindings()
-
-        loginViewModel.loginName.observe(this, android.arch.lifecycle.Observer {
-            editTextLoginName.setText(it)
-        })
-        loginViewModel.loginPass.observe(this, android.arch.lifecycle.Observer {
-            editTextLoginPass.setText(it)
-        })
-        loginViewModel.loginRemember.observe(this, android.arch.lifecycle.Observer {
-            checkBoxLogin.isChecked = it!!
-        })
-
-    }
-
-    private fun loadLoginDetails() {
-        val sharedPreferences = getSharedPreferences(prefsFile, Context.MODE_PRIVATE)
-        loginViewModel.loginRemember.value = sharedPreferences.getBoolean(pref_tag_remember_me, false)
-
-        if (loginViewModel.loginRemember.value!!) {
-            loginViewModel.loginName.value = sharedPreferences.getString(pref_tag_latest_name, "")
-            loginViewModel.loginPass.value = sharedPreferences.getString(pref_tag_latest_pass, "")
-        }
-    }
-
-    private fun saveLoginDetails() {
-        val editor = getSharedPreferences(prefsFile, Context.MODE_PRIVATE).edit()
-        editor.putBoolean(pref_tag_remember_me, loginViewModel.loginRemember.value!!)
-        if (loginViewModel.loginRemember.value!!) {
-            editor.putString(pref_tag_latest_pass, loginViewModel.loginPass.value)
-                    .putString(pref_tag_latest_name, loginViewModel.loginName.value)
-        }
-        editor.apply()
+        supportFragmentManager.beginTransaction()
+                .add(R.id.layoutLoginFragmentContainer, LoginFragment(), LoginFragment.tag)
+                .commit()
     }
 }
---
+++ app/src/main/java/com/example/tim/androidshowcase/Login/LoginFragment.kt
@@ -0,0 +1,115 @@
+package com.example.tim.androidshowcase.Login
+
+import android.app.Activity
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.android.databinding.library.baseAdapters.BR
+import com.example.tim.androidshowcase.MainActivity
+import com.example.tim.androidshowcase.R
+import com.example.tim.androidshowcase.databinding.FragmentLoginLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_login_login.*
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        private const val prefsFile = ".login_prefs"
+        private const val pref_tag_latest_name = "LOGIN_NAME"
+        private const val pref_tag_latest_pass = "LOGIN_PASS"
+        private const val pref_tag_remember_me = "LOGIN_REMEMBER"
+        val tag: String = "LoginFragment"
+    }
+
+    lateinit var loginViewModel: LoginViewModel
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseAuthListener: FirebaseAuth.AuthStateListener
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        loginViewModel = ViewModelProviders.of(activity).get(LoginViewModel::class.java)
+
+        val binding: FragmentLoginLoginBinding = DataBindingUtil.inflate(inflater!!, R.layout.fragment_login_login, container, false)
+        val rootView: View = binding.root
+        binding.setVariable(BR.loginViewModel, loginViewModel)
+        binding.executePendingBindings()
+
+        loginViewModel.loginName.observe(this, android.arch.lifecycle.Observer {
+            editTextLoginName.setText(it)
+        })
+        loginViewModel.loginPass.observe(this, android.arch.lifecycle.Observer {
+            editTextLoginPass.setText(it)
+        })
+        loginViewModel.loginRemember.observe(this, android.arch.lifecycle.Observer {
+            checkBoxLogin.isChecked = it!!
+        })
+
+        val btnLogin: Button = rootView!!.findViewById(R.id.buttonLogin)
+        btnLogin.setOnClickListener {
+            loginViewModel.loginRemember.value = checkBoxLogin.isChecked
+            loginViewModel.loginName.value = editTextLoginName.text.toString()
+            loginViewModel.loginPass.value = editTextLoginPass.text.toString()
+            loginAs(loginViewModel.loginName.value!!, loginViewModel.loginPass.value!!)
+        }
+
+        loadLoginDetails()
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+
+        }
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+    }
+
+    override fun onDestroy() {
+        saveLoginDetails()
+        super.onDestroy()
+    }
+
+    private fun loginAs(name: String, pass: String) {
+        firebaseAuth.signInWithEmailAndPassword(name, pass).addOnCompleteListener(context as Activity, {
+            task -> run {
+            if (task.isSuccessful) {
+                startActivity(Intent(context, MainActivity::class.java))
+            } else {
+                startActivity(Intent(context, MainActivity::class.java))
+                Toast.makeText(this.activity, "Could not login as " + loginViewModel.loginName.value, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        })
+    }
+
+    private fun loadLoginDetails() {
+        val sharedPreferences = this.activity.getSharedPreferences(prefsFile, Context.MODE_PRIVATE)
+        loginViewModel.loginRemember.value = sharedPreferences.getBoolean(pref_tag_remember_me, false)
+
+        if (loginViewModel.loginRemember.value!!) {
+            loginViewModel.loginName.value = sharedPreferences.getString(pref_tag_latest_name, "")
+            loginViewModel.loginPass.value = sharedPreferences.getString(pref_tag_latest_pass, "")
+        }
+    }
+
+    private fun saveLoginDetails() {
+        val editor = this.activity.getSharedPreferences(prefsFile, Context.MODE_PRIVATE).edit()
+        editor.putBoolean(pref_tag_remember_me, loginViewModel.loginRemember.value!!)
+        if (loginViewModel.loginRemember.value!!) {
+            editor.putString(pref_tag_latest_pass, loginViewModel.loginPass.value)
+                    .putString(pref_tag_latest_name, loginViewModel.loginName.value)
+        }
+        editor.apply()
+    }
+}
